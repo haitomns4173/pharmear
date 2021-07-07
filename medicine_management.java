7bbc698bf657e6607b97f00988ae45dba84036a6
Haitomns G <haitomns4173@gmail.com>
2021-07-07 13:29:52 +0545

    Made Card Layout of Internalframe

--- medicine_management.java
+++ medicine_management.java
@@ -1,15 +1,21 @@
 package mms;
 
-import java.awt.Color;
-import javax.swing.ImageIcon;
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 public class medicine_management extends javax.swing.JFrame {
+    
+    
     public medicine_management() {
         initComponents();
-        setExtendedState(medicine_management.MAXIMIZED_BOTH); 
+        setExtendedState(medicine_management.MAXIMIZED_BOTH);
+        
+        BasicInternalFrameUI welcom_titel_hide = (BasicInternalFrameUI)welcomIframe.getUI();
+        welcom_titel_hide.setNorthPane(null);
+        welcomIframe.setVisible(true);
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -20,11 +26,18 @@ public class medicine_management extends javax.swing.JFrame {
         medicine_manage_button = new javax.swing.JButton();
         sales_button = new javax.swing.JButton();
         user_button = new javax.swing.JButton();
-        medicine_management_tpane = new javax.swing.JTabbedPane();
-        billTPane = new javax.swing.JTabbedPane();
-        medicineTpane = new javax.swing.JTabbedPane();
-        salesTpane = new javax.swing.JTabbedPane();
-        userTpane = new javax.swing.JTabbedPane();
+        title_pane = new javax.swing.JPanel();
+        cardPane = new javax.swing.JPanel();
+        welcomIframe = new javax.swing.JInternalFrame();
+        jLabel1 = new javax.swing.JLabel();
+        billIframe = new javax.swing.JInternalFrame();
+        jLabel2 = new javax.swing.JLabel();
+        medicineIframe = new javax.swing.JInternalFrame();
+        jLabel3 = new javax.swing.JLabel();
+        salesIframe = new javax.swing.JInternalFrame();
+        jLabel4 = new javax.swing.JLabel();
+        userIframe = new javax.swing.JInternalFrame();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Pharmacy");
@@ -36,11 +49,11 @@ public class medicine_management extends javax.swing.JFrame {
             }
         });
 
-        background.setBackground(new java.awt.Color(229, 107, 111));
+        background.setBackground(new java.awt.Color(168, 218, 220));
 
-        side_panel_menu.setBackground(new java.awt.Color(109, 89, 122));
+        side_panel_menu.setBackground(new java.awt.Color(29, 53, 87));
 
-        bill_button.setBackground(new java.awt.Color(139, 120, 144));
+        bill_button.setBackground(new java.awt.Color(69, 123, 157));
         bill_button.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         bill_button.setForeground(new java.awt.Color(255, 255, 255));
         bill_button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mms/src/icons8_billing_machine_48px.png"))); // NOI18N
@@ -53,7 +66,7 @@ public class medicine_management extends javax.swing.JFrame {
             }
         });
 
-        medicine_manage_button.setBackground(new java.awt.Color(139, 120, 144));
+        medicine_manage_button.setBackground(new java.awt.Color(69, 123, 157));
         medicine_manage_button.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         medicine_manage_button.setForeground(new java.awt.Color(255, 255, 255));
         medicine_manage_button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mms/src/icons8_pill_45px.png"))); // NOI18N
@@ -66,7 +79,7 @@ public class medicine_management extends javax.swing.JFrame {
             }
         });
 
-        sales_button.setBackground(new java.awt.Color(139, 120, 144));
+        sales_button.setBackground(new java.awt.Color(69, 123, 157));
         sales_button.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         sales_button.setForeground(new java.awt.Color(255, 255, 255));
         sales_button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mms/src/icons8_total_sales_45px.png"))); // NOI18N
@@ -79,7 +92,7 @@ public class medicine_management extends javax.swing.JFrame {
             }
         });
 
-        user_button.setBackground(new java.awt.Color(139, 120, 144));
+        user_button.setBackground(new java.awt.Color(69, 123, 157));
         user_button.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         user_button.setForeground(new java.awt.Color(255, 255, 255));
         user_button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mms/src/icons8_person_45px.png"))); // NOI18N
@@ -107,7 +120,7 @@ public class medicine_management extends javax.swing.JFrame {
         side_panel_menuLayout.setVerticalGroup(
             side_panel_menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(side_panel_menuLayout.createSequentialGroup()
-                .addGap(183, 183, 183)
+                .addGap(111, 111, 111)
                 .addComponent(bill_button)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(medicine_manage_button)
@@ -115,14 +128,143 @@ public class medicine_management extends javax.swing.JFrame {
                 .addComponent(sales_button)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(user_button)
-                .addContainerGap(118, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        medicine_management_tpane.setBackground(new java.awt.Color(229, 107, 111));
-        medicine_management_tpane.addTab("tab1", billTPane);
-        medicine_management_tpane.addTab("tab2", medicineTpane);
-        medicine_management_tpane.addTab("tab3", salesTpane);
-        medicine_management_tpane.addTab("tab4", userTpane);
+        title_pane.setBackground(new java.awt.Color(230, 57, 70));
+
+        javax.swing.GroupLayout title_paneLayout = new javax.swing.GroupLayout(title_pane);
+        title_pane.setLayout(title_paneLayout);
+        title_paneLayout.setHorizontalGroup(
+            title_paneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        title_paneLayout.setVerticalGroup(
+            title_paneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 45, Short.MAX_VALUE)
+        );
+
+        cardPane.setLayout(new java.awt.CardLayout());
+
+        welcomIframe.setBorder(null);
+        welcomIframe.setVisible(true);
+
+        jLabel1.setText("I am welcom");
+
+        javax.swing.GroupLayout welcomIframeLayout = new javax.swing.GroupLayout(welcomIframe.getContentPane());
+        welcomIframe.getContentPane().setLayout(welcomIframeLayout);
+        welcomIframeLayout.setHorizontalGroup(
+            welcomIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(welcomIframeLayout.createSequentialGroup()
+                .addGap(226, 226, 226)
+                .addComponent(jLabel1)
+                .addContainerGap(235, Short.MAX_VALUE))
+        );
+        welcomIframeLayout.setVerticalGroup(
+            welcomIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(welcomIframeLayout.createSequentialGroup()
+                .addGap(111, 111, 111)
+                .addComponent(jLabel1)
+                .addContainerGap(303, Short.MAX_VALUE))
+        );
+
+        cardPane.add(welcomIframe, "card2");
+
+        billIframe.setBorder(null);
+        billIframe.setVisible(true);
+
+        jLabel2.setText("I am bill");
+
+        javax.swing.GroupLayout billIframeLayout = new javax.swing.GroupLayout(billIframe.getContentPane());
+        billIframe.getContentPane().setLayout(billIframeLayout);
+        billIframeLayout.setHorizontalGroup(
+            billIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(billIframeLayout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jLabel2)
+                .addContainerGap(344, Short.MAX_VALUE))
+        );
+        billIframeLayout.setVerticalGroup(
+            billIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(billIframeLayout.createSequentialGroup()
+                .addGap(149, 149, 149)
+                .addComponent(jLabel2)
+                .addContainerGap(265, Short.MAX_VALUE))
+        );
+
+        cardPane.add(billIframe, "card3");
+
+        medicineIframe.setBorder(null);
+        medicineIframe.setVisible(true);
+
+        jLabel3.setText("I am Medicine");
+
+        javax.swing.GroupLayout medicineIframeLayout = new javax.swing.GroupLayout(medicineIframe.getContentPane());
+        medicineIframe.getContentPane().setLayout(medicineIframeLayout);
+        medicineIframeLayout.setHorizontalGroup(
+            medicineIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(medicineIframeLayout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jLabel3)
+                .addContainerGap(315, Short.MAX_VALUE))
+        );
+        medicineIframeLayout.setVerticalGroup(
+            medicineIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(medicineIframeLayout.createSequentialGroup()
+                .addGap(149, 149, 149)
+                .addComponent(jLabel3)
+                .addContainerGap(265, Short.MAX_VALUE))
+        );
+
+        cardPane.add(medicineIframe, "card3");
+
+        salesIframe.setBorder(null);
+        salesIframe.setVisible(true);
+
+        jLabel4.setText("I am sales");
+
+        javax.swing.GroupLayout salesIframeLayout = new javax.swing.GroupLayout(salesIframe.getContentPane());
+        salesIframe.getContentPane().setLayout(salesIframeLayout);
+        salesIframeLayout.setHorizontalGroup(
+            salesIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(salesIframeLayout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jLabel4)
+                .addContainerGap(332, Short.MAX_VALUE))
+        );
+        salesIframeLayout.setVerticalGroup(
+            salesIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(salesIframeLayout.createSequentialGroup()
+                .addGap(149, 149, 149)
+                .addComponent(jLabel4)
+                .addContainerGap(265, Short.MAX_VALUE))
+        );
+
+        cardPane.add(salesIframe, "card3");
+
+        userIframe.setBorder(null);
+        userIframe.setVisible(true);
+
+        jLabel5.setText("I am user");
+
+        javax.swing.GroupLayout userIframeLayout = new javax.swing.GroupLayout(userIframe.getContentPane());
+        userIframe.getContentPane().setLayout(userIframeLayout);
+        userIframeLayout.setHorizontalGroup(
+            userIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(userIframeLayout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jLabel5)
+                .addContainerGap(335, Short.MAX_VALUE))
+        );
+        userIframeLayout.setVerticalGroup(
+            userIframeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(userIframeLayout.createSequentialGroup()
+                .addGap(149, 149, 149)
+                .addComponent(jLabel5)
+                .addContainerGap(265, Short.MAX_VALUE))
+        );
+
+        cardPane.add(userIframe, "card3");
 
         javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
         background.setLayout(backgroundLayout);
@@ -131,13 +273,20 @@ public class medicine_management extends javax.swing.JFrame {
             .addGroup(backgroundLayout.createSequentialGroup()
                 .addComponent(side_panel_menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(medicine_management_tpane, javax.swing.GroupLayout.DEFAULT_SIZE, 534, Short.MAX_VALUE)
+                .addComponent(cardPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(0, 0, 0))
+            .addComponent(title_pane, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         backgroundLayout.setVerticalGroup(
             backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(side_panel_menu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(medicine_management_tpane)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backgroundLayout.createSequentialGroup()
+                .addComponent(title_pane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(side_panel_menu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(backgroundLayout.createSequentialGroup()
+                        .addGap(0, 0, 0)
+                        .addComponent(cardPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(0, 0, 0))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -163,22 +312,66 @@ public class medicine_management extends javax.swing.JFrame {
 
     private void bill_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bill_buttonActionPerformed
         bill_button.setBackground(new Color(229,107,111));
-        medicine_management_tpane.setSelectedIndex(1);
+        medicine_manage_button.setBackground(new Color(69,123,157));
+        sales_button.setBackground(new Color(69,123,157));
+        user_button.setBackground(new Color(69,123,157));
+        
+        BasicInternalFrameUI bill_titel_hide = (BasicInternalFrameUI)billIframe.getUI();
+        bill_titel_hide.setNorthPane(null);
+        
+        welcomIframe.setVisible(false);
+        billIframe.setVisible(true);
+        medicineIframe.setVisible(false);
+        salesIframe.setVisible(false);
+        userIframe.setVisible(false);
     }//GEN-LAST:event_bill_buttonActionPerformed
 
     private void medicine_manage_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_medicine_manage_buttonActionPerformed
+        bill_button.setBackground(new Color(69,123,157));
         medicine_manage_button.setBackground(new Color(229,107,111));
-        medicine_management_tpane.setSelectedIndex(2);
+        sales_button.setBackground(new Color(69,123,157));
+        user_button.setBackground(new Color(69,123,157));
+        
+        BasicInternalFrameUI medicine_titel_hide = (BasicInternalFrameUI)medicineIframe.getUI();
+        medicine_titel_hide.setNorthPane(null);
+        
+        welcomIframe.setVisible(false);
+        billIframe.setVisible(false);
+        medicineIframe.setVisible(true);
+        salesIframe.setVisible(false);
+        userIframe.setVisible(false);
     }//GEN-LAST:event_medicine_manage_buttonActionPerformed
 
     private void sales_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sales_buttonActionPerformed
+        bill_button.setBackground(new Color(69,123,157));
+        medicine_manage_button.setBackground(new Color(69,123,157));
         sales_button.setBackground(new Color(229,107,111));
-        medicine_management_tpane.setSelectedIndex(3);
+        user_button.setBackground(new Color(69,123,157));
+        
+        BasicInternalFrameUI sales_titel_hide = (BasicInternalFrameUI)salesIframe.getUI();
+        sales_titel_hide.setNorthPane(null);
+        
+        welcomIframe.setVisible(false);
+        billIframe.setVisible(false);
+        medicineIframe.setVisible(false);
+        salesIframe.setVisible(true);
+        userIframe.setVisible(false);
     }//GEN-LAST:event_sales_buttonActionPerformed
 
     private void user_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_user_buttonActionPerformed
+        bill_button.setBackground(new Color(69,123,157));
+        medicine_manage_button.setBackground(new Color(69,123,157));
+        sales_button.setBackground(new Color(69,123,157));
         user_button.setBackground(new Color(229,107,111));
-        medicine_management_tpane.setSelectedIndex(4);
+        
+        BasicInternalFrameUI user_titel_hide = (BasicInternalFrameUI)userIframe.getUI();
+        user_titel_hide.setNorthPane(null);
+        
+        welcomIframe.setVisible(false);
+        billIframe.setVisible(false);
+        medicineIframe.setVisible(false);
+        salesIframe.setVisible(false);
+        userIframe.setVisible(true);
     }//GEN-LAST:event_user_buttonActionPerformed
 
     public static void main(String args[]) {
@@ -215,15 +408,22 @@ public class medicine_management extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel background;
-    private javax.swing.JTabbedPane billTPane;
+    private javax.swing.JInternalFrame billIframe;
     private javax.swing.JButton bill_button;
-    private javax.swing.JTabbedPane medicineTpane;
+    private javax.swing.JPanel cardPane;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JInternalFrame medicineIframe;
     private javax.swing.JButton medicine_manage_button;
-    private javax.swing.JTabbedPane medicine_management_tpane;
-    private javax.swing.JTabbedPane salesTpane;
+    private javax.swing.JInternalFrame salesIframe;
     private javax.swing.JButton sales_button;
     private javax.swing.JPanel side_panel_menu;
-    private javax.swing.JTabbedPane userTpane;
+    private javax.swing.JPanel title_pane;
+    private javax.swing.JInternalFrame userIframe;
     private javax.swing.JButton user_button;
+    private javax.swing.JInternalFrame welcomIframe;
     // End of variables declaration//GEN-END:variables
 }
